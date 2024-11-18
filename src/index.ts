2a3582f051530b4d1d87894061b31744b567d9c1
변영화[주문&결제FE] <younghwa.byun@navercorp.com>
2024-11-18 17:48:38 +0900

    ✅ 폴리필 테스트 수정 (findLast)

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export {default as bundleWithVite} from './bundleWithVite.ts'
 export type {BundleWithViteOption} from './bundleWithVite.ts'
 
 // 폴리필 테스트
-// const arr = [1, 2]
-// console.log(arr.at(-1))
+// const array1 = [5, 12, 50, 130, 44]
+// const found = array1.findLast((element) => element > 45)
+// console.log(found)
